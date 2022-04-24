94c9e537cbdf77694410b911af4a0f6b78f1bc7f
atulkumarrai <iamatul1214@gmail.com>
2022-04-24 14:01:43 +0530

    Trying to execute app.py from docker

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
-
+import os
+import pandas as pd
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running"
+    # root = os.getcwd()
+    # print(os.getcwd())
+    # test_dir = os.path.join(os.getcwd(),"Testing_directory")
+    # os.makedirs(test_dir, exist_ok=True)
+    # os.chdir(test_dir)
+    # # os.mkdir("Testing_directory")
+    # # os.chdir("Testing_directory")
+    # data = {'Name': ['Tom', 'Joseph', 'Krish', 'John'], 'Age': [20, 21, 19, 18]}
+    # df = pd.DataFrame(data)
+    # df.to_csv("Testing_df.csv")
+    # df_new = pd.read_csv("Testing_df.csv")
+    # print(df_new)
+    # print(df.shape)
+    # shape = df_new.shape
+    # os.rmdir(os.path.join(root,test_dir))
+    # return f"Flask app is running and shape = {shape}"
+    return "this is just a testing app"
 
 
 if __name__ == "__main__":
