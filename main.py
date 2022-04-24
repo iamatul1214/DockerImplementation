a25be21e8c5148679a99a3ea4da6d830d427095e
atulkumarrai <iamatul1214@gmail.com>
2022-04-24 16:21:22 +0530

    creating folders in runtime

--- main.py
+++ main.py
@@ -22,7 +22,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return f"Flask app is running successfully and below is the json for it {sample_json}"
+    root = os.getcwd()
+    # print(os.getcwd())
+    test_dir = os.path.join(os.getcwd(),"Testing_directory")
+    os.makedirs(test_dir, exist_ok=True)
+    os.chdir(test_dir)
+    # os.mkdir("Testing_directory")
+    # os.chdir("Testing_directory")
+    data = {'Name': ['Tom', 'Joseph', 'Krish', 'John'], 'Age': [20, 21, 19, 18]}
+    df = pd.DataFrame(data)
+    df.to_csv("Testing_df.csv")
+    df_new = pd.read_csv("Testing_df.csv")
+    print(df_new)
+    print(df.shape)
+    shape = df_new.shape
+    # os.rmdir(os.path.join(root,test_dir))
+    return f"Flask app is running and shape = {shape}"
+    # return f"Flask app is running successfully and below is the json for it {sample_json}"
 
 port = int(os.getenv("PORT", 5001))
 
